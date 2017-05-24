2e44afb5536a2ca8ae0ec7910077cad3858f5031
John R Iversen <john@johniversen.org>
2017-05-23 18:49:48 -0700

    fileio/fixsampleinfo.m: temporary check if still have old-style nsi trl
    This change is incomplete--may not be needed, so it just posts an error. If use for a while with no errors, can remove it.

--- fileio/private/fixsampleinfo.m
+++ fileio/private/fixsampleinfo.m
@@ -10,6 +10,14 @@ function data = fixsampleinfo(data)
 
 % Copyright (C) 2009-2020, Robert Oostenveld and Jan-Mathijs Schoffelen
 
+% *** JRI *** check if we've converted old style NSI Trialinfo to new-style
+nsi_trlinfo = ft_findcfg(data.cfg, 'trlinfo'); %*** JRI *** NSI specific trial information (cell)
+if ~isfield(data, 'nsi_trialinfo') && ~isempty(nsi_trlinfo),
+    error('will need to fix this--look at jriChanges.html for code')
+    %/Users/jri/Documents/matlab/matlab/library/contrib/fieldtrip/fieldtrip-temp/jriChanges.html
+end
+% *** JRI ***
+
 if isfield(data, 'sampleinfo')
   % sampleinfo is present (as requested), trialinfo is optional
   return
