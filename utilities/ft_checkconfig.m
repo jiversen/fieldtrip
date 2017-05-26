7a2769555d058c415577b41ce3b1a748675697bb
John R Iversen <john@johniversen.org>
2017-05-25 19:12:27 -0700

    ft_checkconfig: optim: disable checksize always

--- utilities/ft_checkconfig.m
+++ utilities/ft_checkconfig.m
@@ -83,6 +83,10 @@ checkinside     = ft_getopt(varargin, 'inside2logical', 'no');
 checksize       = ft_getopt(varargin, 'checksize', 'no');
 trackconfig     = ft_getopt(varargin, 'trackconfig');
 
+% *** JRI *** maddening, assert a sensible default for me, regardless of
+% caller intention
+checksize = 'off';
+
 if ~isempty(trackconfig) && strcmp(trackconfig, 'on')
   if ft_platform_supports('matlabversion', '2015a', inf)
     % disable config tracking for the time being, due to a known bug (3187)
