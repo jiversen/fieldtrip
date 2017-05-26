94aa9fabad3ddd94eb0953ea7009c05624841604
John R Iversen <john@johniversen.org>
2017-05-25 18:57:33 -0700

    prepare_mesh_headshape, remove_double_vertices: improvement

--- private/remove_double_vertices.m
+++ private/remove_double_vertices.m
@@ -7,11 +7,15 @@ function [posR, triR, keeppos] = remove_double_vertices(pos, tri)
 % Use as
 %   [pos, tri] = remove_double_vertices(pos, tri)
 
-[pos1,i1,i2] = unique(pos, 'rows');
-%keeppos   = find(ismember(pos,pos1,'rows'));
-%removepos = setdiff([1:size(pos,1)],keeppos);
-keeppos   = i1;
+% pos1 = unique(pos, 'rows');
+% keeppos   = find(ismember(pos1,pos,'rows'));
+[pos1, keeppos] = unique(pos, 'rows'); % *** JRI *** improved works
 removepos = setdiff(1:size(pos,1), keeppos);
+
+% *** JRI *** remove NAN points introduced by fairsurface
+tmp = find(any(isnan(pos),2));
+removepos = union(removepos, tmp);
+% *** JRI ***
 
 npos = size(pos,1);
 ntri = size(tri,1);
